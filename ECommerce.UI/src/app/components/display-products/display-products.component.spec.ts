cd6b7c5cb6482aa369e50a5072f6a04d03846ad6
Emmanuel <emmanuelmartinez122623@gmail.com>
2022-11-30 22:30:44 -0700

    Add profile details

--- ECommerce.UI/src/app/components/display-products/display-products.component.spec.ts
+++ ECommerce.UI/src/app/components/display-products/display-products.component.spec.ts
@@ -1,13 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { DisplayProductsComponent } from './display-products.component';
+import { ProductService } from 'src/app/services/product.service';
+import { ProductDetails } from 'src/app/models/ProductDetails/ProductDetails';
+import { observable, Observable } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { compileNgModule } from '@angular/compiler';
+import {By} from "@angular/platform-browser"
 
 describe('DisplayProductsComponent', () => {
   let component: DisplayProductsComponent;
   let fixture: ComponentFixture<DisplayProductsComponent>;
 
+  let mockProductList : ProductDetails[] = [new ProductDetails(1,"test", 100,"test", 2.99, "test", "test", ["test"])]; 
+ 
+  const mockRes: Observable<ProductDetails[]> = new Observable((subscriber) => {
+    setTimeout(() => {
+      subscriber.next(mockProductList);
+      subscriber.complete();
+    });
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports :[HttpClientModule],
       declarations: [ DisplayProductsComponent ]
     })
     .compileComponents();
@@ -22,4 +38,53 @@ describe('DisplayProductsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch product details', fakeAsync(()=> {
+    fixture = TestBed.createComponent(DisplayProductsComponent);
+    component = fixture.componentInstance; 
+    let productService = fixture.debugElement.injector.get(ProductService);
+    let spy = spyOn(productService, 'getProducts').and.returnValue(mockRes)
+    fixture.detectChanges();
+    tick();
+    expect(component.products).toEqual(mockProductList);
+  }))
+
+  it('should render a sidebar', fakeAsync(()=> {
+    const {debugElement} = fixture; 
+    let productService = fixture.debugElement.injector.get(ProductService);
+    let spy = spyOn(productService, 'getProducts').and.returnValue(mockRes)
+    fixture.detectChanges();
+    tick();
+    const sidebar = debugElement.query(By.css('.sidebar'))
+    expect(sidebar).toBeTruthy();
+  }))
+
+  it('should render a page-count', ()=> {
+    let productDetailsCard = document.querySelector(".page-count");
+    expect(productDetailsCard).toBeTruthy();
+
+  })
+
+  it('should render a product container', fakeAsync(()=> {
+    fixture = TestBed.createComponent(DisplayProductsComponent);
+    component = fixture.componentInstance; 
+    let productService = fixture.debugElement.injector.get(ProductService);
+    let spy = spyOn(productService, 'getProducts').and.returnValue(mockRes)
+    fixture.detectChanges();
+    tick();
+    let productDetailsCard = document.querySelector(".container");
+    expect(productDetailsCard).toBeTruthy();
+  }))
+
+  it('should render a product card', fakeAsync(()=> {
+    fixture = TestBed.createComponent(DisplayProductsComponent);
+    component = fixture.componentInstance; 
+    let productService = fixture.debugElement.injector.get(ProductService);
+    let spy = spyOn(productService, 'getProducts').and.returnValue(mockRes)
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    let productDetailsCard = document.querySelector(".cardListItem");
+    expect(productDetailsCard).toBeTruthy();
+  }))
 });
